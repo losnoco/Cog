349a3b0ba2ac822915a68026a59b87e8bdc1201d
vspader <unknown>
2006-04-17 17:32:13 +0000

    Universal Binary

--- Libraries/build_libs.sh
+++ Libraries/build_libs.sh
@@ -1,8 +1,8 @@
-for directory in *
+libs=( DecMPA FAAD2 MAC MPCDec Ogg FLAC Shorten SndFile TagLib Vorbis WavPack )
+
+for lib in "${libs[@]}"
 do
-	if [ -d $directory ]; then
-		cd $directory
-		xcodebuild -alltargets -configuration Release
-		cd ..
-	fi
+	cd $lib
+	xcodebuild -alltargets -configuration Release
+	cd ..
 done
