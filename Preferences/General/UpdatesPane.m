9f094a11de3946e3650f7ed18c6b44fbe5ad0603
vspader <unknown>
2007-02-19 02:57:58 +0000

    Added updates preference pane.

---
+++ Preferences/General/UpdatesPane.m
@@ -0,0 +1,20 @@
+//
+//  FileDrawerPane.m
+//  Preferences
+//
+//  Created by Vincent Spader on 9/4/06.
+//  Copyright 2006 Vincent Spader. All rights reserved.
+//
+
+#import "UpdatesPane.h"
+
+
+@implementation UpdatesPane
+
+- (void)awakeFromNib
+{
+	[self setName:@"Updates"];
+	[self setIcon:@"updates"];
+}
+
+@end
