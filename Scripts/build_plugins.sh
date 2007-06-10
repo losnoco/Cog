60800c900a74f6f74cf244e2787c08ecd72f3087
vspader <unknown>
2007-06-10 23:21:44 +0000

    Added quicktime plugin to build scripts. Moved quicktime to release target.

--- Scripts/build_plugins.sh
+++ Scripts/build_plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-plugins=( CoreAudio MonkeysAudio Musepack Flac Shorten TagLib Vorbis WavPack MAD FileSource HTTPSource)
+plugins=( CoreAudio MonkeysAudio Musepack Flac Shorten TagLib Vorbis WavPack MAD Quicktime FileSource HTTPSource)
 
 for plugin in "${plugins[@]}"
 do
