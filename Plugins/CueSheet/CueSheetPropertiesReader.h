4bcf22ced2ba1dd811027f32cc4f39378f41a287
vspader <unknown>
2007-10-10 03:13:38 +0000

    Whoops, forgot to add a couple cue sheet related files.

---
+++ Plugins/CueSheet/CueSheetPropertiesReader.h
@@ -0,0 +1,18 @@
+//
+//  CueSheetPropertiesReader.h
+//  CueSheet
+//
+//  Created by Vincent Spader on 10/08/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "Plugin.h"
+
+@interface CueSheetPropertiesReader : NSObject <CogPropertiesReader>
+{
+
+}
+
+@end
---
+++ Plugins/CueSheet/CueSheetPropertiesReader.m
@@ -0,0 +1,38 @@
+//
+//  CueSheetPropertiesReader.m
+//  CueSheet
+//
+//  Created by Vincent Spader on 10/08/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "CueSheetPropertiesReader.h"
+#import "CueSheetDecoder.h"
+
+@implementation CueSheetPropertiesReader
+
++ (NSDictionary *)propertiesForSource:(id<CogSource>)source
+{
+	NSDictionary *properties;
+	CueSheetDecoder *decoder;
+	
+	decoder = [[CueSheetDecoder alloc] init];
+	if (![decoder open:source])
+	{
+		return nil;
+	}
+	
+	properties = [decoder properties];
+	
+	[decoder close];
+	
+	return properties;
+}
+
+
++ (NSArray *)fileTypes
+{
+	return [CueSheetDecoder fileTypes];
+}
+
+@end
