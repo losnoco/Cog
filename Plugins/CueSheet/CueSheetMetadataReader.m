8c4f5fa41cf884f3b7ecb9d7db81067795a27f6f
vspader <unknown>
2007-10-13 01:36:42 +0000

    Forgot cuesheet metadata reader files.

---
+++ Plugins/CueSheet/CueSheetMetadataReader.m
@@ -0,0 +1,52 @@
+//
+//  CueSheetMetadataReader.m
+//  CueSheet
+//
+//  Created by Vincent Spader on 10/12/07.
+//  Copyright 2007 __MyCompanyName__. All rights reserved.
+//
+
+#import "CueSheetMetadataReader.h"
+#import "CueSheetDecoder.h"
+
+#import "CueSheet.h"
+#import "CueSheetTrack.h"
+
+@implementation CueSheetMetadataReader
+
++ (NSArray *)fileTypes
+{
+	return [CueSheetDecoder fileTypes];
+}
+
++ (NSDictionary *)metadataForURL:(NSURL *)url
+{
+	if (![url isFileURL]) {
+		return nil;
+	}
+
+	CueSheet *cuesheet = [CueSheet cueSheetWithFile:[url path]];
+
+	NSArray *tracks = [cuesheet tracks];
+	CueSheetTrack *track;
+	NSEnumerator *e = [tracks objectEnumerator];
+	while (track = [e nextObject])
+	{
+		if ([[url fragment] isEqualToString:[track track]])
+		{
+			return [NSDictionary dictionaryWithObjectsAndKeys:
+				[track artist], @"artist",
+				[track album], @"album",
+				[track title], @"title",
+				[track track], @"track",
+				[track genre], @"genre",
+				[track year], @"year",
+				nil];
+		
+		}
+	}
+
+	return nil;
+}
+
+@end
