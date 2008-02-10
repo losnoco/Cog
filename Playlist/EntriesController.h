803a3306a563543f9789b75d5b0ce7bf677e392f
vspader <unknown>
2008-02-10 22:55:16 +0000

    Forgot to add these files for undo support.

---
+++ Playlist/EntriesController.h
@@ -0,0 +1,23 @@
+//
+//  EntriesController.h
+//  Cog
+//
+//  Created by Vincent Spader on 2/10/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "PlaylistEntry.h"
+
+@interface EntriesController : NSObject {
+	NSUndoManager *undoManager;
+	NSMutableArray *playlistEntries;
+}
+
+- (NSMutableArray *)entries;
+- (void)setEntries:(NSMutableArray *)array;
+- (void)insertObject:(PlaylistEntry *)pe inEntriesAtIndex:(int)index;
+- (void)removeObjectFromEntriesAtIndex:(int)index;
+
+
+@end
