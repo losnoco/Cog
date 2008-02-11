ca76dbab7de8a17495f5b63ba0cad8223e3cea2c
matthewleon <unknown>
2008-02-11 18:51:51 +0000

    Added necessary transformers for Spotlight path.

---
+++ SpotlightTransformers.m
@@ -0,0 +1,30 @@
+//
+//  SpotlightTransformers.m
+//  Cog
+//
+//  Created by Matthew Grinshpun on 11/02/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#include <Cocoa/Cocoa.h>
+
+@interface StringToURLTransformer: NSValueTransformer {}
+@end
+@implementation StringToURLTransformer
++ (Class)transformedValueClass { return [NSURL class]; }
++ (BOOL)allowsReverseTransformation { return YES; }
+
+// Convert from string to NSURL
+- (id)transformedValue:(id)value {
+    if (value == nil) return nil;
+    
+    return [NSURL URLWithString:value];
+}
+
+// Convert from NSURL to string
+- (id)reverseTransformedValue:(id)value {
+    if (value == nil) return nil;
+    
+    return [value absoluteString];
+}
+@end
