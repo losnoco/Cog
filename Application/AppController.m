477bdbd4efaaf8e556df2cd8e1bc437470d911cf
vspader <unknown>
2008-03-05 01:22:42 +0000

    Fixed strange whitespace issue. Also, increased bottom border a bit to make it match other status bar heights.

--- Application/AppController.m
+++ Application/AppController.m
@@ -228,7 +228,7 @@ increase/decrease as long as the user holds the left/right, plus/minus button */
 - (void)awakeFromNib
 {
 	[mainWindow setExcludedFromWindowsMenu:YES];
-	[mainWindow setContentBorderThickness:22.0 forEdge:NSMinYEdge];
+	[mainWindow setContentBorderThickness:24.0 forEdge:NSMinYEdge];
 
 	[[totalTimeField cell] setBackgroundStyle:NSBackgroundStyleRaised];
 	
