bedc3a2adee4de7caf1c184f1c86c21ef1c4db09
vspader <unknown>
2008-05-01 23:35:39 +0000

    Updated tracking slider so it works again. The time is updated while dragging, and seeking is only done when the mouse button is released.

--- Utils/TrackingSlider.m
+++ Utils/TrackingSlider.m
@@ -3,34 +3,6 @@
 
 @implementation TrackingSlider
 
-- (id)initWithCoder:(NSCoder *)decoder
-{
-	self = [super initWithCoder:decoder];
-	if (self)
-	{
-		if (![[self cell] isKindOfClass:[TrackingCell class]])
-		{
-			TrackingCell *trackingCell;
-			trackingCell = [[TrackingCell alloc] init];
-			
-			[trackingCell setControlSize:[[self cell] controlSize]];
-			[trackingCell setAction:[[self cell] action]];
-			[trackingCell setContinuous:[[self cell] isContinuous]];
-			[trackingCell setTarget:[[self cell] target]];
-			[self setCell:trackingCell];
-			
-			[trackingCell release];
-		}
-	}
-	
-	return self;
-}
-
-+ (Class) cellClass
-{
-	return [TrackingCell class];
-}
-
 -(BOOL)tracking
 {
 	return [[self cell] tracking];
