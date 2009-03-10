0d1398e0fbb95b20b82175d987a14702e86414ae
vspader <unknown>
2009-03-09 21:59:27 -0700

    Added ToolTipTextField class, whos tooltip is the same as the text displayed, but untruncated.

---
+++ Utils/ToolTipTextField.h
@@ -0,0 +1,16 @@
+//
+//  ToolTipTextField.h
+//  Cog
+//
+//  Created by Vincent Spader on 3/9/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface ToolTipTextField : NSTextField {
+
+}
+
+@end
---
+++ Utils/ToolTipTextField.m
@@ -0,0 +1,28 @@
+//
+//  ToolTipTextField.m
+//  Cog
+//
+//  Created by Vincent Spader on 3/9/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "ToolTipTextField.h"
+
+
+@implementation ToolTipTextField
+
+- (void)setObjectValue:(id)obj
+{
+	NSString *string = [obj description];
+	
+	NSFormatter *formatter = [[self cell] formatter];
+	if (nil != formatter) {
+		string = [formatter stringForObjectValue:obj];
+	}	
+	
+	[self setToolTip:string];
+	
+	[super setObjectValue:obj];
+}
+
+@end
