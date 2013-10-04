8ac55b2e2739195b361436817eaf18838a315a75
Chris Moeller <kode54@gmail.com>
2013-10-04 10:54:40 -0700

    Added missing files and updated .hgignore file

---
+++ Preferences/General/VolumeBehaviorArrayController.h
@@ -0,0 +1,13 @@
+//
+//  VolumeBehaviorArrayController.h
+//  General
+//
+//  Created by Christopher Snowhill on 10/1/13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VolumeBehaviorArrayController : NSArrayController
+
+@end
---
+++ Preferences/General/VolumeBehaviorArrayController.m
@@ -0,0 +1,47 @@
+//
+//  VolumeBehaviorArrayController.m
+//  General
+//
+//  Created by Christopher Snowhill on 10/1/13.
+//
+//
+
+#import "VolumeBehaviorArrayController.h"
+
+@implementation VolumeBehaviorArrayController
+- (void)awakeFromNib
+{
+	[self removeObjects:[self arrangedObjects]];
+    
+	[self addObject:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      NSLocalizedStringFromTableInBundle(@"ReplayGain Album Gain with peak", nil, [NSBundle bundleForClass:[self class]], @"") , @"name",
+      @"albumGainWithPeak", @"preference",nil]];
+    
+	[self addObject:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      NSLocalizedStringFromTableInBundle(@"ReplayGain Album Gain", nil, [NSBundle bundleForClass:[self class]], @"") , @"name",
+      @"albumGain", @"preference",nil]];
+    
+	[self addObject:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      NSLocalizedStringFromTableInBundle(@"ReplayGain Track Gain with peak", nil, [NSBundle bundleForClass:[self class]], @"") , @"name",
+      @"trackGainWithPeak", @"preference",nil]];
+    
+	[self addObject:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      NSLocalizedStringFromTableInBundle(@"ReplayGain Track Gain", nil, [NSBundle bundleForClass:[self class]], @"") , @"name",
+      @"trackGain", @"preference",nil]];
+    
+	[self addObject:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      NSLocalizedStringFromTableInBundle(@"Volume scale tag only", nil, [NSBundle bundleForClass:[self class]], @"") , @"name",
+      @"volumeScale", @"preference",nil]];
+    
+	[self addObject:
+     [NSDictionary dictionaryWithObjectsAndKeys:
+      NSLocalizedStringFromTableInBundle(@"No volume scaling", nil, [NSBundle bundleForClass:[self class]], @"") , @"name",
+      @"none", @"preference",nil]];
+}
+
+@end
