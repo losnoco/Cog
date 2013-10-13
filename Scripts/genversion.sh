ee29955db061547bc31859ea1cce0abf0cf4d488
Chris Moeller <kode54@gmail.com>
2013-10-12 17:53:59 -0700

    Fixed build version info generator

--- Scripts/genversion.sh
+++ Scripts/genversion.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-hg_version=$(hg log -r . --template '{latesttag}-{latesttagdistance}-{node|short}')
+hg_version=$(/usr/local/bin/hg log -r . --template '{latesttag}-{latesttagdistance}-{node|short}')
 
 build_time=$(date)
 
-info_plist="${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/Info.plist"
+echo "hg_version=${hg_version}"
+echo "build_time=${build_time}"
+
+info_plist="${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion '${hg_version}'" "${info_plist}"
 /usr/libexec/PlistBuddy -c "Add :BuildTime date '${build_time}'" "${info_plist}"
 
+exit 0
