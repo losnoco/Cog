22e7474e0e238470b85175c2f622fa39ee63a12f
Chris Moeller <kode54@gmail.com>
2013-10-17 23:55:38 -0700

    Fixed compiling the debug build; stupid search path behavior...

--- Frameworks/FLAC/flac-1.2.1/include/FLAC/assert.h
+++ Frameworks/FLAC/flac-1.2.1/include/FLAC/assert.h
@@ -34,7 +34,7 @@
 
 /* we need this since some compilers (like MSVC) leave assert()s on release code (and we don't want to use their ASSERT) */
 #ifdef DEBUG
-#include <assert.h>
+#include </usr/include/assert.h>
 #define FLAC__ASSERT(x) assert(x)
 #define FLAC__ASSERT_DECLARATION(x) x
 #else
