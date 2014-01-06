060c0ebc27c8e35a9962b6ec792f6ce2d78fece9
Chris Moeller <kode54@gmail.com>
2014-01-05 19:10:19 -0800

    Updated DUMB's unmo3 library to version 2.4.0.5, which fixes decoding MP3 compressed samples

--- ThirdParty/BASS/unmo3.h
+++ ThirdParty/BASS/unmo3.h
@@ -14,13 +14,13 @@ extern "C" {
 #endif
 
 // Get the UNMO3 library version
-DWORD WINAPI UNMO3_GetVersion();
+unsigned WINAPI UNMO3_GetVersion();
 
 // Decode a MO3 file
 // in: data/len = MO3 data/len
 // out: data/len = decoded data/len (if successful)
 // return: 0 = Success, 2 = It isn't a MO3 file, 3 = There was a problem decoding the MO3 file
-int WINAPI UNMO3_Decode(void **data, int *len, DWORD flags);
+int WINAPI UNMO3_Decode(void **data, unsigned *len, unsigned flags);
 
 // UNMO3_Decode flags
 #define UNMO3_DECODE_NOSAMPLES	1 // don't process sample data
