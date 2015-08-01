fdefcf4128434ff02373a1a23b91722a079d2e79
Chris Moeller <kode54@gmail.com>
2015-07-31 23:48:08 -0700

    Updated genversion.sh for new Git repository

--- Scripts/genversion.sh
+++ Scripts/genversion.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-hg_version=$(/usr/local/bin/hg log -r . --template '{latesttag}-{latesttagdistance}-{node|short}')
+git_version=$(/usr/bin/git describe --tags)
 
 build_time=$(date)
 
-echo "hg_version=${hg_version}"
+echo "git_version=${git_version}"
 echo "build_time=${build_time}"
 
 info_plist="${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion '${hg_version}'" "${info_plist}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion '${git_version}'" "${info_plist}"
 /usr/libexec/PlistBuddy -c "Add :BuildTime date '${build_time}'" "${info_plist}"
 
 exit 0
