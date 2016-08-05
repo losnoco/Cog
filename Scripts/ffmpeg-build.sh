5f8bcf8ff11014aa8f08a438839e011b06bd49bc
Christopher Snowhill <kode54@gmail.com>
2016-08-05 16:40:48 -0700

    Update and fix FFmpeg, which fixes an MP3 parser crash.

--- Scripts/ffmpeg-build.sh
+++ Scripts/ffmpeg-build.sh
@@ -5,28 +5,23 @@
     --disable-ffprobe --disable-ffserver --disable-avdevice --disable-ffmpeg\
     --disable-postproc --disable-swresample --disable-avfilter\
     --disable-swscale --enable-network --disable-swscale-alpha --disable-vdpau\
-    --disable-dxva2 --enable-hwaccels\
-    --disable-encoders --disable-muxers --disable-indevs --disable-outdevs\
-    --disable-devices --disable-filters --disable-parsers --enable-parser=ac3\
-    --enable-parser=mpegaudio\
-    --enable-demuxer=ac3 --disable-bsfs --disable-bzlib --disable-protocols\
-    --disable-decoders --disable-decoder=libopus --disable-libopus\
-    --enable-decoder=wmapro --enable-decoder=wmav1 --enable-decoder=wmav2\
-    --enable-decoder=wmavoice --enable-decoder=wmalossless --enable-decoder=xma1\
-    --enable-decoder=xma2 --enable-decoder=dca --enable-decoder=ac3\
-    --disable-decoder=amrnb\
-    --disable-demuxers --enable-demuxer=asf --enable-demuxer=xwma\
-    --enable-demuxer=mov --enable-demuxer=oma --enable-demuxer=ac3\
-    --disable-demuxer=amr --enable-demuxer=ogg\
-    --enable-demuxer=tak --enable-decoder=tak\
-    --enable-decoder=dsd_lsbf --enable-decoder=dsd_lsbf_planar\
-    --enable-decoder=dsd_msbf --enable-decoder=dsd_msbf_planar\
-    --enable-demuxer=dsf --enable-demuxer=wav\
+    --disable-dxva2 --disable-everything --enable-hwaccels\
+    --enable-parser=ac3 --enable-demuxer=ac3 --enable-decoder=ac3\
+    --enable-parser=mpegaudio --enable-decoder=wmapro --enable-decoder=wmav1\
+    --enable-decoder=wmav2 --enable-decoder=wmavoice --enable-decoder=wmalossless
+    --enable-decoder=xma1 --enable-decoder=xma2 --enable-demuxer=asf
+    --enable-demuxer=xwma --enable-decoder=dca --enable-demuxer=mov\
+    --enable-demuxer=oma --enable-demuxer=ogg --enable-demuxer=tak\
+    --enable-decoder=tak --enable-decoder=dsd_lsbf
+    --enable-decoder=dsd_lsbf_planar --enable-decoder=dsd_msbf
+    --enable-decoder=dsd_msbf_planar --enable-demuxer=dsf --enable-demuxer=wav\
     --enable-demuxer=aac --enable-decoder=aac\
     --enable-decoder=atrac3 --enable-decoder=atrac3p\
     --enable-demuxer=dts --enable-demuxer=dtshd\
     --enable-demuxer=mp3 --enable-decoder=mp3float\
-    --disable-libopencore-amrnb --disable-libopencore-amrwb\
-    --disable-version3
+    --disable-parser=mpeg4video --disable-parser=h263\
+    --disable-decoder=mpeg2video --disable-decoder=h263 --disable-decoder=h264\
+    --disable-decoder=mpeg1video --disable-decoder=mpeg2video\
+    --disable-decoder=mpeg4 --disable-version3
 
 make -j8
