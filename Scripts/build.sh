4ea71d96f08e12226e0c83b6495fadfbc7ffe7ee
Christopher Snowhill <kode54@gmail.com>
2017-12-27 19:20:19 -0800

    Remove last traces of Travis CI support.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,23 +4,7 @@ pushd $(dirname $0)
 BASE=`pwd -P`
 popd
 
-sign=true
-
-while getopts ":hn" option; do
-  case $option in
-    h) echo "usage: $0 [-h] [-n]"; exit ;;
-    n) sign=false ;;
-    ?) echo "error: option -$OPTARG is not implemented"; exit ;;
-  esac
-done
-
-SIGNARGS=""
-
-if [ "$sign" = true ] ; then
-  SIGNARGS=('CODE_SIGN_IDENTITY=""' 'CODE_SIGNING_REQUIRED=NO')
-fi
-
 BUILDPRODUCTS="$BASE"/build/Build/Products/Release
 
-xcodebuild -quiet -workspace "$BASE"/../Cog.xcodeproj/project.xcworkspace -scheme Cog -configuration Release -derivedDataPath "$BASE"/build ${SIGNARGS[*]}
+xcodebuild -quiet -workspace "$BASE"/../Cog.xcodeproj/project.xcworkspace -scheme Cog -configuration Release -derivedDataPath "$BASE"/build
 
