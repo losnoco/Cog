aaaa5d574fdc041104682967b288fbb42e90b533
Christopher Snowhill <kode54@gmail.com>
2018-01-04 18:00:39 -0800

    Replaced DUMB and company with libopenmpt.

---
+++ Plugins/OpenMPT/OpenMPT/OMPTMetadataReader.h
@@ -0,0 +1,17 @@
+//
+//  OMPTMetadataReader.h
+//  OpenMPT
+//
+//  Created by Christopher Snowhill on 1/4/18.
+//  Copyright 2018 __LoSnoCo__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "Plugin.h"
+
+@interface OMPTMetadataReader : NSObject <CogMetadataReader> {
+
+}
+
+@end
