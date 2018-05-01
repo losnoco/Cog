d8e0ab71f6e7c79ead65d819a11622e826fa05b6
Chris Moeller <kode54@gmail.com>
2018-05-01 00:33:37 -0700

    Remove CDN reset as we no longer use CloudFront.

--- Scripts/update_feed.rb
+++ Scripts/update_feed.rb
@@ -115,7 +115,4 @@ if appcast_revision < latest_revision
 
   #Upload to S3
   %x[s3cmd put -P -m application/xml '#{site_dir}/#{feed}_builds/#{feed}.xml' s3://balde.losno.co/cog/ --signature-v2]
-
-  #Invalidate CDN
-  %x[aws cloudfront create-invalidation --distribution-id E1WUF11KRNP2VH --paths /cog/mercury.xml]
 end
