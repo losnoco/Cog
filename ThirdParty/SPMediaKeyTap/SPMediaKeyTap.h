18176782e705bfcc06baab74daaab1d6769dd9f0
Christopher Snowhill <kode54@gmail.com>
2018-06-09 19:22:04 -0700

    Update SPMediaKeyTap to a more recent version.

--- ThirdParty/SPMediaKeyTap/SPMediaKeyTap.h
+++ ThirdParty/SPMediaKeyTap/SPMediaKeyTap.h
@@ -4,18 +4,19 @@
 
 // http://overooped.com/post/2593597587/mediakeys
 
-#define SPSystemDefinedEventMediaKeys 8
+#define SPSystemDefinedEventMediaKeys       8
+#define SPPassthroughEventData2Value        -10
 
 @interface SPMediaKeyTap : NSObject {
-	EventHandlerRef _app_switching_ref;
-	EventHandlerRef _app_terminating_ref;
-	CFMachPortRef _eventPort;
-	CFRunLoopSourceRef _eventPortSource;
-	CFRunLoopRef _tapThreadRL;
-	BOOL _shouldInterceptMediaKeyEvents;
-	id _delegate;
-	// The app that is frontmost in this list owns media keys
-	NSMutableArray *_mediaKeyAppList;
+       EventHandlerRef _app_switching_ref;
+       EventHandlerRef _app_terminating_ref;
+       CFMachPortRef _eventPort;
+       CFRunLoopSourceRef _eventPortSource;
+       CFRunLoopRef _tapThreadRL;
+       BOOL _shouldInterceptMediaKeyEvents;
+       id _delegate;
+       // The app that is frontmost in this list owns media keys
+       NSMutableArray *_mediaKeyAppList;
 }
 + (NSArray*)defaultMediaKeyUserBundleIdentifiers;
 
@@ -25,6 +26,9 @@
 -(void)startWatchingMediaKeys;
 -(void)stopWatchingMediaKeys;
 -(void)handleAndReleaseMediaKeyEvent:(NSEvent *)event;
+
+@property NSArray *blackListBundleIdentifiers;
+
 @end
 
 @interface NSObject (SPMediaKeyTapDelegate)
@@ -36,6 +40,7 @@ extern "C" {
 #endif
 
 extern NSString *kMediaKeyUsingBundleIdentifiersDefaultsKey;
+extern NSString *kMediaKeyUsingBlackListBundleIdentifiersDefaultsKey;
 extern NSString *kIgnoreMediaKeysDefaultsKey;
 
 #ifdef __cplusplus
