7657eb49718786a15d554f3a371e302e697daa8d
Christopher Snowhill <kode54@gmail.com>
2019-07-02 15:54:31 -0700

    Updated VGMStream to r1050-2345-g1d28e0f5

--- Frameworks/vgmstream/vgmstream/src/vgmstream.h
+++ Frameworks/vgmstream/vgmstream/src/vgmstream.h
@@ -742,6 +742,7 @@ typedef enum {
     meta_MSF_KONAMI,
     meta_XWMA_KONAMI,
     meta_9TAV,
+    meta_BWAV,
 
 } meta_t;
 
@@ -1042,6 +1043,7 @@ typedef struct {
     int interleave; /* size of stream interleave */
     int encryption; /* encryption mode */
     int big_endian;
+    int skip_samples;
     /* for AHX */
     int cri_type;
     uint16_t cri_key1;
