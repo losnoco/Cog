258a88464aca84021b8d778bb26efd6cf6924a0d
Christopher Snowhill <kode54@gmail.com>
2019-11-08 15:05:21 -0800

    Fix VGM metadata and properties reading

---
+++ Plugins/GME/GamePropertiesReader.h
@@ -0,0 +1,17 @@
+//
+//  GamePropertiesReader.h
+//  GME
+//
+//  Created by Christopher Snowhill on 11/08/19.
+//  Copyright 2019 __LoSnoCo__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import "Plugin.h"
+
+@interface GamePropertiesReader : NSObject <CogPropertiesReader> {
+
+}
+
+@end
---
+++ Plugins/GME/GamePropertiesReader.m
@@ -0,0 +1,45 @@
+//
+//  VGMPropertiesReader.m
+//  VGMStream
+//
+//  Created by Christopher Snowhill on 10/18/19.
+//  Copyright 2019 __LoSnoCo__. All rights reserved.
+//
+
+#import "GamePropertiesReader.h"
+#import "GameDecoder.h"
+
+@implementation GamePropertiesReader
+
++ (NSArray *)fileTypes
+{
+	return [GameDecoder fileTypes];
+}
+
++ (NSArray *)mimeTypes
+{
+	return [GameDecoder mimeTypes];
+}
+
++ (float)priority
+{
+    return [GameDecoder priority];
+}
+
++ (NSDictionary *)propertiesForSource:(id<CogSource>)source
+{
+    GameDecoder * decoder = [[GameDecoder alloc] init];
+    
+    NSDictionary * properties = [NSDictionary dictionary];
+    
+    if ([decoder open:source])
+    {
+        properties = [decoder properties];
+        
+        [decoder close];
+    }
+    
+    return properties;
+}
+
+@end
