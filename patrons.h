cbe7dcfd4f603b40cec56c96eaf6ad827af3e494
Christopher Snowhill <kode54@gmail.com>
2020-05-02 00:58:26 -0700

    Add patrons to credits info

---
+++ patrons.h
@@ -0,0 +1,10 @@
+#ifndef PATRONS_H
+#define PATRONS_H
+
+#define MY_PATRONS \
+"Thanks to my patrons:\n" \
+"Joseph DeVore, Hongfei Gu, Alexander Pecheny, Ceeb, Electric Keet,\n" \
+"Miles Rotaru, Yuri Kunde Schlesner, govt man, Antti Aro,\n" \
+"happyvideogamemusicnerd"
+
+#endif
