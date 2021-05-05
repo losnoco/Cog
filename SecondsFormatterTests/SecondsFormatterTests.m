c020867bcef6cd8bced6f581050a2a644d75c11f
Jan Weiß <jan@geheimwerk.de>
2021-05-05 14:58:03 +0200

    Pouring foundation for SecondsFormatterTests.

--- SecondsFormatterTests/SecondsFormatterTests.m
+++ SecondsFormatterTests/SecondsFormatterTests.m
@@ -7,18 +7,24 @@
 
 #import <XCTest/XCTest.h>
 
+#import "SecondsFormatter.h"
+
 @interface SecondsFormatterTests : XCTestCase
 
 @end
 
 @implementation SecondsFormatterTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+- (void)setUp
+{
+	[super setUp];
+	// Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)tearDown
+{
+	// Put teardown code here. This method is called after the invocation of each test method in the class.
+	[super tearDown];
 }
 
 - (void)testExample {
@@ -26,11 +32,14 @@
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+#if 0
+- (void)testPerformanceExample
+{
+	// This is an example of a performance test case.
+	[self measureBlock:^{
+		// Put the code you want to measure the time of here.
+	}];
 }
+#endif
 
 @end
