d18c9c9bb05a89eb6ea9b0e982b048a58a59f331
Calvin Buckley <calvin@cmpct.info>
2021-08-08 17:57:42 -0300

    Why was this commented out?

--- Application/PlaybackController.m
+++ Application/PlaybackController.m
@@ -617,7 +617,7 @@ NSDictionary * makeRGInfo(PlaylistEntry *pe)
 	
 	[self setPlaybackStatus:status];
     // If we don't send it here, if we've stopped, then the NPIC will be stuck at the last file we played.
-    //[self sendMetaData];
+    [self sendMetaData];
 }
 
 - (void)playlistDidChange:(PlaylistController *)p
