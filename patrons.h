9edf4ddc49fd2b03623ed6ab839fbeb70ddc21db
Christopher Snowhill <kode54@gmail.com>
2022-01-26 17:19:50 -0800

    Update credits: New patron
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- patrons.h
+++ patrons.h
@@ -4,6 +4,6 @@
 #define MY_PATRONS \
 "Thanks to my patrons:\n" \
 "Alexander Pecheny, Electric Keet, govt man, Antti Aro,\n"
-"Fjölnir Ásgeirsson"
+"Fjölnir Ásgeirsson, Giampaolo Bellavite"
 
 #endif
