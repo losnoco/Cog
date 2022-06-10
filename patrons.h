6156aa0095f85a7d1396d62724e3c5dfab271376
Christopher Snowhill <kode54@gmail.com>
2022-06-10 00:30:18 -0700

    [Credits] Update Patreon credits
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- patrons.h
+++ patrons.h
@@ -3,7 +3,7 @@
 
 #define MY_PATRONS            \
 	"Thanks to my patrons:\n" \
-	"Alexander Pecheny, Electric Keet, govt man, Antti Aro,\n"
+	"Alexander Pecheny, Electric Keet, Antti Aro,\n"
 "Fj\xc3\xb6lnir \xc3\x81sgeirsson, Giampaolo Bellavite"
 
 #endif
