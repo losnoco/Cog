574365287930079878b98acdd365a81ce39eb546
Christopher Snowhill <kode54@gmail.com>
2023-02-05 16:46:31 -0800

    Update copyright year in various places
    Update these things a bit for the next release.
    
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- Plugins/libvgmPlayer/libvgmMetadataReader.h
+++ Plugins/libvgmPlayer/libvgmMetadataReader.h
@@ -3,7 +3,7 @@
 //  libvgmPlayer
 //
 //  Created by Christopher Snowhill on 1/03/22.
-//  Copyright 2022 __LoSnoCo__. All rights reserved.
+//  Copyright 2022-2023 __LoSnoCo__. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
