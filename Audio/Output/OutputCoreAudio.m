d6b7ed467ecdab826227e92900810db77898ba93
Christopher Snowhill <kode54@gmail.com>
2025-02-11 18:12:06 -0800

    Output: Remove pointless scale value
    This shouldn't have been applied, the problem with Rubber Band was the
    flushing mechanism.
    
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- Audio/Output/OutputCoreAudio.m
+++ Audio/Output/OutputCoreAudio.m
@@ -418,7 +418,7 @@ current_device_listener(AudioObjectID inObjectID, UInt32 inNumberAddresses, cons
 }
 
 - (BOOL)processEndOfStream {
-	if(stopping || ([outputController endOfStream] == YES && [self signalEndOfStream:secondsLatency / tempo])) {
+	if(stopping || ([outputController endOfStream] == YES && [self signalEndOfStream:secondsLatency])) {
 		stopping = YES;
 		return YES;
 	}
