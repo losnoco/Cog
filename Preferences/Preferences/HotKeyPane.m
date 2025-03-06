04db82d0ffe03e488372d507cdf7018db2e3dfd9
Christopher Snowhill <kode54@gmail.com>
2025-03-06 03:08:41 -0800

    Bug Fix: Move default hotkeys to main app startup
    Instead of the Preferences plugin.
    
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- Preferences/Preferences/HotKeyPane.m
+++ Preferences/Preferences/HotKeyPane.m
@@ -9,68 +9,13 @@
 #import "HotKeyPane.h"
 #import "Shortcuts.h"
 
-MASShortcut *shortcutWithMigration(NSString *oldKeyCodePrefName,
-                                   NSString *oldKeyModifierPrefName,
-                                   NSString *newShortcutPrefName,
-                                   NSInteger newDefaultKeyCode) {
-	NSEventModifierFlags defaultModifiers = NSEventModifierFlagControl | NSEventModifierFlagCommand;
-	NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-	if([defaults objectForKey:oldKeyCodePrefName]) {
-		NSInteger oldKeyCode = [defaults integerForKey:oldKeyCodePrefName];
-		NSEventModifierFlags oldKeyModifiers = [defaults integerForKey:oldKeyModifierPrefName];
-		// Should we consider temporarily save these values for further migration?
-		[defaults removeObjectForKey:oldKeyCodePrefName];
-		[defaults removeObjectForKey:oldKeyModifierPrefName];
-		return [MASShortcut shortcutWithKeyCode:oldKeyCode modifierFlags:oldKeyModifiers];
-	} else {
-		return [MASShortcut shortcutWithKeyCode:newDefaultKeyCode modifierFlags:defaultModifiers];
-	}
-}
-
 @implementation HotKeyPane {
 	NSUserDefaultsController *defaultsController;
 }
 
+// Defaults have been moved to AppController.m
 - (void)awakeFromNib {
-	MASShortcut *playShortcut = shortcutWithMigration(@"hotKeyPlayKeyCode",
-	                                                  @"hotKeyPlayModifiers",
-	                                                  CogPlayShortcutKey,
-	                                                  kVK_ANSI_P);
-	MASShortcut *nextShortcut = shortcutWithMigration(@"hotKeyNextKeyCode",
-	                                                  @"hotKeyNextModifiers",
-	                                                  CogNextShortcutKey,
-	                                                  kVK_ANSI_N);
-	MASShortcut *prevShortcut = shortcutWithMigration(@"hotKeyPreviousKeyCode",
-	                                                  @"hotKeyPreviousModifiers",
-	                                                  CogPrevShortcutKey,
-	                                                  kVK_ANSI_R);
-	MASShortcut *spamShortcut = shortcutWithMigration(@"hotKeySpamKeyCode",
-	                                                  @"hotKeySpamModifiers",
-	                                                  CogSpamShortcutKey,
-	                                                  kVK_ANSI_C);
-	MASShortcut *fadeShortcut = shortcutWithMigration(@"hotKeyFadeKeyCode",
-													  @"hotKeyFadeModifiers",
-													  CogFadeShortcutKey,
-													  kVK_ANSI_O);
-
-	NSData *playShortcutData = [NSKeyedArchiver archivedDataWithRootObject:playShortcut];
-	NSData *nextShortcutData = [NSKeyedArchiver archivedDataWithRootObject:nextShortcut];
-	NSData *prevShortcutData = [NSKeyedArchiver archivedDataWithRootObject:prevShortcut];
-	NSData *spamShortcutData = [NSKeyedArchiver archivedDataWithRootObject:spamShortcut];
-	NSData *fadeShortcutData = [NSKeyedArchiver archivedDataWithRootObject:fadeShortcut];
-
-	// Register default values to be used for the first app start
-	NSDictionary<NSString *, NSData *> *defaultShortcuts = @{
-		CogPlayShortcutKey: playShortcutData,
-		CogNextShortcutKey: nextShortcutData,
-		CogPrevShortcutKey: prevShortcutData,
-		CogSpamShortcutKey: spamShortcutData,
-		CogFadeShortcutKey: fadeShortcutData
-	};
-
-	defaultsController =
-	[[NSUserDefaultsController sharedUserDefaultsController] initWithDefaults:nil
-	                                                            initialValues:defaultShortcuts];
+	defaultsController = [NSUserDefaultsController sharedUserDefaultsController];
 
 	_playShortcutView.associatedUserDefaultsKey = CogPlayShortcutKey;
 	_nextShortcutView.associatedUserDefaultsKey = CogNextShortcutKey;
