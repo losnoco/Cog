ecf0028c9d52ba97f3f8bc3caef3f6448e061020
Christopher Snowhill <kode54@gmail.com>
2025-09-07 23:57:54 -0700

    VGMStream: Updated libvgmstream code base
    Updated VGMStream to r2023-182-g6dedf2b5
    
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- Frameworks/vgmstream/vgmstream/src/meta/hca_keys.h
+++ Frameworks/vgmstream/vgmstream/src/meta/hca_keys.h
@@ -1577,6 +1577,12 @@ static const hcakey_info hcakey_list[] = {
 
     // Aether Gazer (multi)
     {1578660190369042886},      // 15E887143BFF51C6
+
+    // Sonic Racing CrossWorlds (multi)
+    {32105414741057402},        // 00720FB46101DF7A
+
+    // Ride Kamens (iOS/Android)
+    {20000608},                 // 0000000001312F60
 };
 
 #endif
