459c6e2f96a59c96194783aea7444c2487d93d36
Christopher Snowhill <kode54@gmail.com>
2025-09-26 02:55:01 -0700

    Nuked SC-55: Make changes for LCD state export
    And also rendering capability.
    
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- Frameworks/nuked-sc55/nuked-sc55/lcd.h
+++ Frameworks/nuked-sc55/nuked-sc55/lcd.h
@@ -38,11 +38,11 @@
 
 #include "state.h"
 
-int LCD_SetBack(struct sc55_state *st, const char *name, sc55_read_rom readCallback, void *readContext);
+//int LCD_SetBack(struct sc55_state *st, const char *name, sc55_read_rom readCallback, void *readContext);
 void LCD_Init(struct sc55_state *st);
-void LCD_UnInit(struct sc55_state *st);
+//void LCD_UnInit(struct sc55_state *st);
 void LCD_Write(struct sc55_state *st, uint32_t address, uint8_t data);
 void LCD_Enable(struct sc55_state *st, uint32_t enable);
 //bool LCD_QuitRequested(struct sc55_state *st);
-void LCD_Sync(struct sc55_state *st);
-void LCD_Update(struct sc55_state *st);
+//void LCD_Sync(struct sc55_state *st);
+void LCD_Update(const struct lcd_state_t *state, const lcd_background_t lcd_background, lcd_buffer_t lcd_buffer);
