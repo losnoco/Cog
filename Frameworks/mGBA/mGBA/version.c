83cb7dce12f5892902d11ebe513890363b365359
Christopher Snowhill <kode54@gmail.com>
2025-10-02 23:54:32 -0700

    Highly Complete: Update libmGBA to a newer version
    Update mGBA to verbatim version 0.4.0-5658-gf6b1854c3
    
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

---
+++ Frameworks/mGBA/mGBA/version.c
@@ -0,0 +1,14 @@
+/* Copyright (c) 2013-2016 Jeffrey Pfau
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#include <mgba/core/version.h>
+
+MGBA_EXPORT const char* const gitCommit = "f6b1854c373fd7cdf8571b9d8568f68bc2decdb1";
+MGBA_EXPORT const char* const gitCommitShort = "f6b1854c3";
+MGBA_EXPORT const char* const gitBranch = "master";
+MGBA_EXPORT const int gitRevision = 5658;
+MGBA_EXPORT const char* const binaryName = "mGBA";
+MGBA_EXPORT const char* const projectName = "libmGBA";
+MGBA_EXPORT const char* const projectVersion = "0.4.0-5658-gf6b1854c3";
