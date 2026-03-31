21597c310fa32cb66ef2c6d9909046b8969f2f00
Leonardo Lobato <leo@lobato.org>
2026-04-01 00:38:15 +0200

    Add KeychainHelper for Last.FM session storage

---
+++ Scrobbler/KeychainHelper.swift
@@ -0,0 +1,46 @@
+import Foundation
+import Security
+
+enum KeychainHelper {
+    private static let service = "com.vinyllo.Cog.lastfm"
+    private static let account = "sessionKey"
+
+    static func save(sessionKey: String) -> Bool {
+        guard let data = sessionKey.data(using: .utf8) else { return false }
+        delete()
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecValueData as String: data,
+        ]
+        return SecItemAdd(query as CFDictionary, nil) == errSecSuccess
+    }
+
+    static func loadSessionKey() -> String? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne,
+        ]
+        var result: AnyObject?
+        guard SecItemCopyMatching(query as CFDictionary, &result) == errSecSuccess,
+              let data = result as? Data,
+              let key = String(data: data, encoding: .utf8) else {
+            return nil
+        }
+        return key
+    }
+
+    @discardableResult
+    static func delete() -> Bool {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrService as String: service,
+            kSecAttrAccount as String: account,
+        ]
+        return SecItemDelete(query as CFDictionary) == errSecSuccess
+    }
+}
