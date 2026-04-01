7bff25b5ef6585e8c8961882aacc1c742991d623
Christopher Snowhill <kode54@gmail.com>
2026-04-01 03:10:19 -0700

    Normalize the indenting for the rest of the file
    This was what Xcode did after applying my edits, it normalized the space
    indenting to tabs, like my settings were supposed to be in the first
    place.
    
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- Preferences/Panes/ShortcutField.swift
+++ Preferences/Panes/ShortcutField.swift
@@ -3,17 +3,17 @@ import SwiftUI
 /// Wraps MASShortcutView for use in SwiftUI.
 /// MASShortcutView manages its own UserDefaults binding internally via `associatedUserDefaultsKey`.
 struct ShortcutField: NSViewRepresentable {
-    let userDefaultsKey: String
-
-    func makeNSView(context: Context) -> MASShortcutView {
-        let view = MASShortcutView()
-        view.associatedUserDefaultsKey = userDefaultsKey
-        return view
-    }
-
-    func updateNSView(_ nsView: MASShortcutView, context: Context) {
-        nsView.associatedUserDefaultsKey = userDefaultsKey
-    }
+	let userDefaultsKey: String
+	
+	func makeNSView(context: Context) -> MASShortcutView {
+		let view = MASShortcutView()
+		view.associatedUserDefaultsKey = userDefaultsKey
+		return view
+	}
+	
+	func updateNSView(_ nsView: MASShortcutView, context: Context) {
+		nsView.associatedUserDefaultsKey = userDefaultsKey
+	}
 	
 	static func dismantleNSView(_ nsView: MASShortcutView, coordinator: ()) {
 		nsView.associatedUserDefaultsKey = nil
