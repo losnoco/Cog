a4d46a6c4f62e35a6e4e0e33114aacab4e991691
Christopher Snowhill <kode54@gmail.com>
2026-04-18 07:19:42 -0700

    MIDI: Reverted external file bank offset detection
    It breaks GBA Mus Ripper rips. Bleh.
    
    See: 731c19eb8310
    Signed-off-by: Christopher Snowhill <kode54@gmail.com>

--- Plugins/MIDI/MIDI/MIDIDecoder.mm
+++ Plugins/MIDI/MIDI/MIDIDecoder.mm
@@ -274,8 +274,6 @@ static OSType getOSType(const char *in_) {
 				}
 			}
 
-			spessaplayer->setFileBankOffset(midi_file.scan_for_bank_offset());
-
 			if([soundFontPath length])
 				spessaplayer->setFileSoundFont([soundFontPath UTF8String]);
 
