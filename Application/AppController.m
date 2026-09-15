b7373d80ca618fb6ea189e4096ca4956b715f60a
Christopher Snowhill <kode54@gmail.com>
2026-09-15 01:06:49 -0700

    Sentry: Enable MetricKit support
    Again, Sentry is opt in, so this shouldn't be anything major. I hope.

--- Application/AppController.m
+++ Application/AppController.m
@@ -390,6 +390,9 @@ static BOOL consentLastEnabled = NO;
 					// Disable hang detection, lots of false positives still
 					options.enableAppHangTracking = NO;
 
+					// Enable MetricKit
+					options.enableMetricKit = YES;
+
 					// Enable logging
 					options.enableLogs = YES;
 
